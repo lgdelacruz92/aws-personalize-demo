56113687d52fa37de842830624c9c07092a4c212
Lester <lgdelacruz92@gmail.com>
2024-08-21 11:00:47 -0400

    using boto3

--- personalize_tut.py
+++ personalize_tut.py
@@ -5,3 +5,8 @@ import numpy as np
 import pandas as pd
 import time
 import datetime
+
+
+# Configure the SDK to Personalize:
+personalize = boto3.client('personalize')
+personalize_runtime = boto3.client('personalize-runtime')
